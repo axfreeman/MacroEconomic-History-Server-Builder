3f3f468c1833496470831ab8088724525ebd0d1a
Alan Freeman <tech@iwgvt.org>
2020-08-22 15:22:31 -0500

    Tidy
    Added little package and sql script to delete all the unwanted ROLAP views (NB not yet deleting the functions, TBD)

---
+++ SETUP/drop_unwanted_tables_and_views.sql
@@ -0,0 +1,34 @@
+-- script to clean up macrohistory_rolap  after finishing ROLAP transfer
+
+DROP TABLE IF EXISTS MADDISON_TD_File;
+DROP TABLE IF EXISTS UN_TD_File;
+DROP TABLE IF EXISTS WID_TD_File;
+DROP VIEW IF EXISTS AddedDefinitions;
+DROP VIEW IF EXISTS Barro_Ursua_cleaner;
+DROP VIEW IF EXISTS Barro_Ursua_Indexed_gdp;
+DROP VIEW IF EXISTS BARRO_URSUA_WDI2018_Deindexed;
+DROP VIEW IF EXISTS IFSSeasonalAdjustmentRemoval;
+DROP VIEW IF EXISTS IFSSeasonalAdjustmentRemoval_KeyFinder;
+DROP VIEW IF EXISTS Macrohist_cleaner;
+DROP VIEW IF EXISTS Macrohist_Indexed_rgdppc;
+DROP VIEW IF EXISTS Macrohist_WDI2018_Deindexed;
+DROP VIEW IF EXISTS Maddison_Population;
+DROP VIEW IF EXISTS MADDISON_RU_Corrector;
+DROP VIEW IF EXISTS MADDISON_RU_Corrector_KeyFinder;
+DROP VIEW IF EXISTS NormalisedAddedDefinitions;
+DROP VIEW IF EXISTS PENN_ExchangeRates;
+DROP VIEW IF EXISTS UN_RU_Corrector;
+DROP VIEW IF EXISTS UN_RU_Corrector_KeyFinder;
+DROP VIEW IF EXISTS WDI2018_2005_REALGDP_BASE;
+DROP VIEW IF EXISTS WDI2018_2006_REALGDP_BASE;
+DROP VIEW IF EXISTS WID_Foreign_Income_USD_Calculated;
+DROP VIEW IF EXISTS WID_Foreign_Income_USD_Calculated_KeyFinder;
+DROP VIEW IF EXISTS WID_National_Income;
+DROP VIEW IF EXISTS WID_National_Income_Calculated;
+DROP VIEW IF EXISTS WID_National_Income_Calculated_KeyFinder;
+DROP VIEW IF EXISTS WID_National_Income_USD_Calculated;
+DROP VIEW IF EXISTS WID_National_Income_USD_Calculated_KeyFinder;
+DROP VIEW IF EXISTS WID_NDP_USD_Calculated;
+DROP VIEW IF EXISTS WID_RU_Corrector;
+DROP VIEW IF EXISTS WID_RU_Corrector_KeyFinder;
+DROP VIEW IF EXISTS WID_USD_Calculated_KeyFinder;
