5eff1ce4a75c9f7f021a4c882e34f2d10e2d8154
Alan Freeman <tech@iwgvt.org>
2020-12-10 17:24:07 -0600

    Maddison
    Maddison 2020 uploaded and tested
    Some changes to geography

--- SETUP/define_PENN_USD_calculator.sql
+++ SETUP/define_PENN_USD_calculator.sql
@@ -1,12 +1,17 @@
 USE macrohistory_oltp
 GO
--- Add USD measures for the various PENN series
+-- Add USD measures for the PENN series
 -- Do it here for convenience rather than oblige the user to write complex
 -- DAX or MDX Queries
 -- create temporary tables so we can inspect them to see what goes on, in case of problems
 
 DROP TABLE IF EXISTS Temp_PENN_USD_Table 
 GO 
+
+-- xr2 is PENN's series code for 'Exchange rate, national currency/USD (market+estimated)'
+-- xr (which we don't use) is their code for 'Exchange rate, national currency/USD (market)'
+-- v_gdp is their code for GDP at current national prices.
+
 
 SELECT
    dbo.RecognisedFacts.SourceName,
@@ -23,7 +28,7 @@ FROM
       and RecognisedFacts.Date = RecognisedFacts_1.Date 
 WHERE
    (
-      dbo.RecognisedFacts.SourceName = N'PENN'
+      dbo.RecognisedFacts.SourceName = N'PENN National Accounts 2020'
    )
    AND 
    (
